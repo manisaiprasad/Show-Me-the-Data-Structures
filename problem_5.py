f0168cfdce7cea7c8f71f1c982cba53f3b9058c8
manisaiprasadam <manisaiprasadam@gmail.com>
2020-04-10 02:09:06 +0530

    changes added

--- problem_5.py
+++ problem_5.py
@@ -1,5 +1,6 @@
+  
+import datetime
 import hashlib
-import time
 
 
 class Block:
@@ -13,33 +14,46 @@ class Block:
     def calc_hash(self):
         sha = hashlib.sha256()
 
-        hash_str = str(self.data).encode('utf-8')
+        hash_str = "We are going to encode this string of data!".encode('utf-8')
 
         sha.update(hash_str)
 
         return sha.hexdigest()
 
-    def __str__(self):
-        return ('Timestamp: {}\nData: {}\nPrevious Hash: {}\nHash: {}'.format(self.timestamp, self.data, self.previous_hash, self.hash))
 
-class Blockchain:
-
+class LinkedList:
     def __init__(self):
-        self.current_block = None
+        self.head = None
+        self.last = None
 
-    def add_block(self, value):
-        timestamp = time.gmtime()
-        data = value
-        previous_hash = self.current_block.hash if self.current_block else 0
-        self.current_block = Block(timestamp, data, previous_hash)
+    def append(self, timestamp, data):
+        if not self.head:
+            self.head = Block(timestamp, data, 0)
+            self.last = self.head
+        else:
+            temp_data = self.last
+            self.last = Block(timestamp, data, temp_data)
+            self.last.previous_hash = temp_data
 
-blockchain = Blockchain()
 
-blockchain.add_block(1)
-print(blockchain.current_block)
+def get_timestamp():
+    return datetime.datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S")
 
-blockchain.add_block(2)
-print(blockchain.current_block)
 
-blockchain.add_block(3)
-print(blockchain.current_block)
+# blocks
+block_zero = Block(get_timestamp(), "Information X", 0)
+block_one = Block(get_timestamp(), "Information Y", block_zero)
+block_two = Block(get_timestamp(), "Information Z", block_one)
+
+# linked list
+linked_list = LinkedList()
+linked_list.append(get_timestamp(), "Information A")
+linked_list.append(get_timestamp(), "Information B")
+
+# tests
+print("Block Zero data : ", block_zero.data)
+print("Block Zero hash : ", block_zero.hash)
+print("Block Zero timestamp : ", block_zero.timestamp)
+print("Block one's previous block's data : ", block_one.previous_hash.data)
+print("Linked list last data : ", linked_list.last.data)
+print("Linked list last's previous hash data : ", linked_list.last.previous_hash.data)
